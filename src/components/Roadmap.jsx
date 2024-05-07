7e44e92bf716ca77f4251b806ae2987cb7d2025f
imranhwebdev <alimrancv870@gmail.com>
2024-05-07 13:18:25 +0600

    update

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -1,32 +1,15 @@
-import {React, useEffect,  useRef, useState } from 'react'
-import AOS from 'aos';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Mousewheel } from 'swiper/modules';
+import {React } from 'react'
 import 'swiper/css';
 import { Col, Container, Row } from 'react-bootstrap'
 import roadMapShap from "../assets/img/roadMapShap.png"
 import roadmapBubleShap from "../assets/img/roadmapboubleShap.png"
 export default function Roadmap() {
-    const sectionAboveSwiperRef = useRef(null);
-    const nextSectionRef = useRef(null);
-
-    const handleSwiperReachEnd = () => {
-      if (nextSectionRef.current) {
-        nextSectionRef.current.scrollIntoView({ behavior: 'smooth' });
-      }
-    };
-  
-    const handleSwiperReachBeginning = () => {
-      if (sectionAboveSwiperRef.current) {
-        sectionAboveSwiperRef.current.scrollIntoView({ behavior: 'smooth', block: 'end' });
-      }
-    };
   return (
     <section className='roadmap'>
         <figure className='roadmapBubleShap'>
             <img src={roadmapBubleShap} alt="" />
         </figure>
-      <Container  ref={sectionAboveSwiperRef}>
+      <Container>
         <Row>
           <Col>
             <div className="section-title mb-5 pb-3 text-center text-md-start"  data-aos="fade-up" data-aos-duration="1000">
@@ -38,33 +21,11 @@ export default function Roadmap() {
 
       <div className="roadmap-content">
         <Container>
+          <figure className="roadmapShap">
+            <img src={roadMapShap} alt="" />
+          </figure>
           <Row>
-            <Swiper
-              direction={'horizontal'}
-              centeredSlides={true}
-              slidesPerView={4}
-              spaceBetween={20}        
-              mousewheel={true}           
-              modules={[Mousewheel ]}
-              className="mySwiper theseaWhels3Img"
-              onReachEnd={handleSwiperReachEnd}
-              onReachBeginning={handleSwiperReachBeginning}
-              breakpoints={{
-                '@0.00': {
-                slidesPerView: 1,
-                },
-                '@0.75': {
-                slidesPerView: 2,
-                },
-                '@1.00': {
-                slidesPerView: 3,
-                },
-                '@1.50': {
-                slidesPerView: 3,
-                },
-            }}
-          >
-          <SwiperSlide className="about-sinlge-item">
+            <Col lg={4}>
               <div className="roadmap-content-left">
                 <div className="roadmap-single-item">
                   <h4>A New Era</h4>
@@ -79,9 +40,9 @@ export default function Roadmap() {
                   <p>Access To An Exclusive Community, Free Mints, Upcoming Projects, And Much More.</p>
                 </div>
               </div>
-          </SwiperSlide>
-            <SwiperSlide className="about-sinlge-item">
-                <div className="roadmap-content-left">
+            </Col>
+            <Col lg={4}>
+              <div className="roadmap-content-left">
                   <div className="roadmap-single-item">
                     <h4>The Foes</h4>
                     <p>Launch of ‘Hunters’ NFTs – mercenaries that want to kill The Sei Whales and collect their blubber (BLUB).</p>
@@ -94,10 +55,11 @@ export default function Roadmap() {
                     <h4>Play Time</h4>
                     <p>A mini game for NFT holders play and earn BLUB tokens. For expanded game, users can fight to earn prizes and enhance their NFTs.</p>
                   </div>
-                </div>
-            </SwiperSlide>
-            <SwiperSlide className="about-sinlge-item">
-                <div className="roadmap-content-left">
+              </div>
+            </Col>
+
+            <Col lg={4}>
+              <div className="roadmap-content-left">
                   <div className="roadmap-single-item">
                     <h4>The Merch</h4>
                     <p>Marketplace With Members Exclusive Merchandise.</p>
@@ -106,15 +68,10 @@ export default function Roadmap() {
                     <h4>The Revenge</h4>
                     <p>After Years Of Being Hunted, The Sei Whales Put Their Final Plans Into Action.</p>
                   </div>
-                </div>
-            </SwiperSlide>
-
-        </Swiper>
+              </div>     
+            </Col>                         
           </Row>
         </Container>
-      </div>
-
-      <div ref={nextSectionRef}>
       </div>
     </section>
   )
